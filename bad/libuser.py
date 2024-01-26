7fed0451d894d348a5ffd58a236bbf45086b5ca2
Stefan Streichsbier <stefan@streichsbier.at>
2024-01-26 11:31:21 +0800

    Update libuser.py

--- bad/libuser.py
+++ bad/libuser.py
@@ -44,12 +44,21 @@ def userlist():
 
 
 def password_change(username, password):
+    # Issue
     subprocess.Popen([password], shell=1-1)
+    # Issue
     subprocess.Popen([password], shell=True)
+    # Issue
     subprocess.call([password], shell=True)
+    # Issue
     subprocess.run([password], shell=0+1)
+    # Issue
     subprocess.run(username + password, shell=2-3)
+    # Not an issue
     subprocess.run(username + password, shell=2-2)
+    # Not an issue
+    subprocess.run(["{}/Triton/ensemble/fil_backend/scripts/convert_sklearn".format(password), model_path
+  + "/sci_1.pkl"])
     conn = sqlite3.connect('db_users.sqlite')
     conn.set_trace_callback(print)
     conn.row_factory = sqlite3.Row
