f4a2d510884cd7e0092543d213b2138440999782
Stefan Streichsbier <stefan@streichsbier.at>
2024-01-26 11:01:04 +0800

    Update libuser.py

--- bad/libuser.py
+++ bad/libuser.py
@@ -44,7 +44,7 @@ def userlist():
 
 
 def password_change(username, password):
-
+    subprocess.Popen([username], shell=1-1)
     conn = sqlite3.connect('db_users.sqlite')
     conn.set_trace_callback(print)
     conn.row_factory = sqlite3.Row
