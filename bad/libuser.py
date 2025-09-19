a224ce1519fe639a4ed06ce9f2e96fefa3396a57
Stefan Streichsbier <stefan@streichsbier.at>
2025-09-19 18:13:15 +0700

    Update libuser.py

--- bad/libuser.py
+++ bad/libuser.py
@@ -1,28 +1,38 @@
 import sqlite3
-import libuser
+import hashlib
 
 
-def login(username, password):
+def login_user(username, password):
 
-    conn = sqlite3.connect('db_users.sqlite')
+    conn = sqlite3.connect("db_users.sqlite")
     conn.set_trace_callback(print)
     conn.row_factory = sqlite3.Row
     c = conn.cursor()
 
-    user = c.execute("SELECT * FROM users WHERE username = '{}' and password = '{}'".format(username, password)).fetchone()
+    user = c.execute(
+        "SELECT * FROM users WHERE username = '{}' and password = '{}'".format(
+            username, password
+        )
+    ).fetchone()
 
     if user:
-        return user['username']
+        return user["username"]
     else:
         return False
 
 
 def create(username, password):
 
-    conn = sqlite3.connect('db_users.sqlite')
+    conn = sqlite3.connect("db_users.sqlite")
     c = conn.cursor()
 
-    c.execute("INSERT INTO users (username, password, failures, mfa_enabled, mfa_secret) VALUES ('%s', '%s', '%d', '%d', '%s')" %(username, password, 0, 0, ''))
+    gravatar = hashlib.md5(username.encode()).hexdigest()
+
+    c.execute(
+        "INSERT INTO users (username, password, failures, mfa_enabled, mfa_secret, gravatar) "
+        + "VALUES (?, ?, ?, ?, ?, ?)",
+        (username, password, 0, 0, "", gravatar),
+    )
 
     conn.commit()
     conn.close()
@@ -30,7 +40,7 @@ def create(username, password):
 
 def userlist():
 
-    conn = sqlite3.connect('db_users.sqlite')
+    conn = sqlite3.connect("db_users.sqlite")
     conn.set_trace_callback(print)
     conn.row_factory = sqlite3.Row
     c = conn.cursor()
@@ -40,17 +50,21 @@ def userlist():
     if not users:
         return []
     else:
-        return [ user['username'] for user in users ]
+        return [user["username"] for user in users]
 
 
-def password_change(username, password):
+def password_change(username, new_password):
 
-    conn = sqlite3.connect('db_users.sqlite')
+    conn = sqlite3.connect("db_users.sqlite")
     conn.set_trace_callback(print)
     conn.row_factory = sqlite3.Row
     c = conn.cursor()
 
-    c.execute("UPDATE users SET password = '{}' WHERE username = '{}'".format(password, username))
+    c.execute(
+        "UPDATE users SET password = '{}' WHERE username = '{}'".format(
+            new_password, username
+        )
+    )
     conn.commit()
 
     return True
@@ -59,3 +73,13 @@ def password_change(username, password):
 def password_complexity(password):
     return True
 
+
+def delete_user(username):
+
+    conn = sqlite3.connect("db_users.sqlite")
+    c = conn.cursor()
+
+    c.execute("DELETE FROM users WHERE username = ?", (username,))
+
+    conn.commit()
+    conn.close()
